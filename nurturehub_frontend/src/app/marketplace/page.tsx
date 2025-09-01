22efb5a3788044266f16452232e1b7aeb14da82e
MD Rashed Karim <rashed@MDs-MacBook-Pro.local>
2025-09-02 00:36:35 +0600

    minor changes

--- nurturehub_frontend/src/app/marketplace/page.tsx
+++ nurturehub_frontend/src/app/marketplace/page.tsx
@@ -2,6 +2,7 @@ import MarketplaceView from "./view";
 import { getMarketPosts } from "./service";
 export default async function MarketplacePage() {
   const posts = await getMarketPosts();
+  posts.reverse();
   return (
     <div>
       <MarketplaceView data={{ posts }} />
